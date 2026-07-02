b200f8adc5ab7a35e9674dfa0b035eddd8aba664
emidm28052004-droid <emidm28052004@gmail.com>
2026-07-02 18:23:49 -0300

    penultimo update

--- main/JS/galeria.js
+++ main/JS/galeria.js
@@ -1,57 +1,57 @@
 const botones = document.querySelectorAll(".categoria");
 const imagenes = {
   todos: [
-    "http://herreriagys.com.uy/imgFolder/Galeria/decoraciones/decoracion3.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/escaleras/escalera1.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/mobiliarios/mobiliario2.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/puertasYportones/porton2.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/techos/techo1.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/ventanas/ventana1.webp"
+    "https://herreriagys.com.uy/imgFolder/Galeria/decoraciones/decoracion3.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/escaleras/escalera1.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/mobiliarios/mobiliario2.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/puertasYportones/porton2.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/techos/techo1.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/ventanas/ventana1.webp"
   ],
   decoraciones: [
-    "http://herreriagys.com.uy/imgFolder/Galeria/decoraciones/decoracion1.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/decoraciones/decoracion2.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/decoraciones/decoracion3.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/decoraciones/decoracion4.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/decoraciones/decoracion1.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/decoraciones/decoracion2.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/decoraciones/decoracion3.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/decoraciones/decoracion4.webp",
     "#",
     "#"
   ],
   escaleras: [
-    "http://herreriagys.com.uy/imgFolder/Galeria/escaleras/escalera1.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/escaleras/escalera2.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/escaleras/escalera3.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/escaleras/escalera4.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/escaleras/escalera1.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/escaleras/escalera2.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/escaleras/escalera3.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/escaleras/escalera4.webp",
     "#",
     "#"
   ],
   mobiliarios: [
-    "http://herreriagys.com.uy/imgFolder/Galeria/mobiliarios/mobiliario1.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/mobiliarios/mobiliario2.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/mobiliarios/mobiliario3.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/mobiliarios/mobiliario4.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/mobiliarios/mobiliario5.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/mobiliarios/mobiliario6.webp"
+    "https://herreriagys.com.uy/imgFolder/Galeria/mobiliarios/mobiliario1.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/mobiliarios/mobiliario2.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/mobiliarios/mobiliario3.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/mobiliarios/mobiliario4.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/mobiliarios/mobiliario5.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/mobiliarios/mobiliario6.webp"
   ],
   puertasYportones: [
-    "http://herreriagys.com.uy/imgFolder/Galeria/puertasYportones/porton1.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/puertasYportones/porton2.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/puertasYportones/porton3.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/puertasYportones/puerta1.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/puertasYportones/puerta2.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/puertasYportones/puerta3.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/puertasYportones/porton1.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/puertasYportones/porton2.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/puertasYportones/porton3.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/puertasYportones/puerta1.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/puertasYportones/puerta2.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/puertasYportones/puerta3.webp",
   ],
   techos: [
-    "http://herreriagys.com.uy/imgFolder/Galeria/techos/techo1.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/techos/techo2.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/techos/techo3.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/techos/techo4.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/techos/techo1.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/techos/techo2.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/techos/techo3.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/techos/techo4.webp",
     "#",
     "#"
   ],
   ventanas: [
-    "http://herreriagys.com.uy/imgFolder/Galeria/ventanas/ventana1.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/ventanas/ventana2.webp",
-    "http://herreriagys.com.uy/imgFolder/Galeria/ventanas/ventana3.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/ventanas/ventana1.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/ventanas/ventana2.webp",
+    "https://herreriagys.com.uy/imgFolder/Galeria/ventanas/ventana3.webp",
     "#",
     "#",
     "#"
